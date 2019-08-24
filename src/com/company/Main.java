55560cea72e719dd2c79e94dd617fe14e7e5d6af
Rahim <rahim@MacBook-Pro-Rahim.local>
2019-08-24 15:06:04 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Rahim";
+        System.out.println(name);
+        int age = 21;
+        System.out.println(age);
+        int temp = 35;
+        System.out.println(temp);
+        if (temp > 30 && temp < -20 || age > 20 || age < 45) {
+            System.out.println("Не выходим Гулять");
+        } else {
+            System.out.println(("Идем в гости к Другу"));
+        }
+        if (age < 20 || temp > 0 || temp < 28) {
+            System.out.println("Не выходим Гулять!");
+        } else {
+            System.out.println("Идем в гости к Другу");
+        }
+        if (age > 45 || temp > -10 || temp < 25) {
+            System.out.println("Не Выходим Гулять.");
+        } else {
+            System.out.println("Идем в гости к Другу");
+        }
+    }
+}
